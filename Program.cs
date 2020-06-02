50889385ca5c21354722337fae1f0587fa6b950d
kov3372 <oleg.koval921@gmail.com>
2020-06-02 17:01:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            string[] pathForFile = File.ReadAllLines(fileName);
+            Student[] ArrayOfStudent = new Student[pathForFile.Length];
+
+            for (int i = 0; i < pathForFile.Length; i++)
+            {
+                ArrayOfStudent[i] = new Student(pathForFile[i]);
+            }
+            return ArrayOfStudent;
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].scholarship == 0)
+                {
+                    string mathematicsStr = char.ToString(studs[i].mathematicsMark);
+                    string physicsStr = char.ToString(studs[i].physicsMark);
+                    string informaticsStr = char.ToString(studs[i].informaticsMark);
+
+                    int mathematics = Convert.ToInt32(mathematicsStr);
+                    int physics = Convert.ToInt32(physicsStr);
+                    int informatics = Convert.ToInt32(informaticsStr);
+
+                    double averengeMark = (double)(mathematics + physics + informatics) / 3;
+                    Console.WriteLine(" {0}  {1}  {2}  {3} - середній бал ", studs[i].surName, studs[i].firstName, studs[i].patronymic, averengeMark);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Admin\Desktop\input.txt";
+
+            Student[] studs = ReadData(path);
+
+            runMenu(studs);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            string[] linesWiThDataAboutOneStudent = lineWithAllData.Split(" ");
+
+            this.surName = linesWiThDataAboutOneStudent[0];
+            this.firstName = linesWiThDataAboutOneStudent[1];
+            this.patronymic = linesWiThDataAboutOneStudent[2];
+            this.sex = Convert.ToChar(linesWiThDataAboutOneStudent[3]);
+            this.dateOfBirth = linesWiThDataAboutOneStudent[4];
+            this.mathematicsMark = Convert.ToChar(linesWiThDataAboutOneStudent[5]);
+            this.physicsMark = Convert.ToChar(linesWiThDataAboutOneStudent[6]);
+            this.informaticsMark = Convert.ToChar(linesWiThDataAboutOneStudent[7]);
+            this.scholarship = Convert.ToInt32(linesWiThDataAboutOneStudent[8]);
+        }
+    }
+}
